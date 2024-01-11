e90fb7e9b20044c1fc289b8c96c25fe92391a5a7
Vitthal Popat Gund <97181033+VitthalGund@users.noreply.github.com>
2024-01-11 18:34:16 +0000

    Update 4 files

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import { useContext, useEffect } from 'react';
-import './index.css'
-import Sidebar from './components/Sidebar';
-import Highlights from './components/Highlights';
-import WeatherContext from './context/Data/userContext';
-import Forcast from './components/Forcast';
-import { weather, location } from './api/axios';
-import { Toaster } from 'react-hot-toast';
-import toast from 'react-hot-toast';
+import { useContext, useEffect } from "react";
+import "./index.css";
+import Sidebar from "./components/Sidebar";
+import Highlights from "./components/Highlights";
+import WeatherContext from "./context/Data/userContext";
+import Forcast from "./components/Forcast";
+import { weather, location } from "./api/axios";
+import { Toaster } from "react-hot-toast";
+import toast from "react-hot-toast";
 
 function App() {
-
   const {
     locationInfo,
     weatherData,
@@ -18,8 +17,8 @@ function App() {
     setLocationInfo,
     changeBackground,
     setLoading,
-    loading
-  } = useContext(WeatherContext)
+    loading,
+  } = useContext(WeatherContext);
 
   const getWeatherData = async (city) => {
     try {
@@ -27,22 +26,28 @@ function App() {
         return;
       }
 
-      setLoading(true)
-      console.log(import.meta.env)
-      // 
-      const resp = await toast.promise(weather.get(`/weather?q=${city}&units=metric&appid=${import.meta.process.env.apiKey}`), {
-        error: "invalid city",
-        loading: "find your city",
-        success: `Here is weather details of ${city}`
-      })
+      setLoading(true);
+      //
+      const resp = await toast.promise(
+        weather.get(
+          `/weather?q=${city}&units=metric&appid=${
+            import.meta.process.env.apiKey
+          }`,
+        ),
+        {
+          error: "invalid city",
+          loading: "find your city",
+          success: `Here is weather details of ${city}`,
+        },
+      );
       if (!resp.data) {
-        return
+        return;
       }
-      const data = resp.data
+      const data = resp.data;
       setLocationInfo({
         city,
-      })
-      return ({
+      });
+      return {
         city: city,
         temperature: data.main.temp,
         condition: data.weather[0].description,
@@ -55,61 +60,57 @@ function App() {
         humidity: data.main.humidity,
         pressure: data.main.pressure,
         visibility: data.visibility,
-        clouds: data.clouds.all // Per
-      })
+        clouds: data.clouds.all, // Per
+      };
     } catch (error) {
       console.log("invalid city name");
       getLiveLocation().then((data) => {
-        setLocationInfo(data)
-        setLoading(true)
+        setLocationInfo(data);
+        setLoading(true);
         getWeatherData(locationInfo.city).then((data) => {
           if (!data) {
-            return
+            return;
           }
-          console.log(data)
-          setWeatherData(data)
-          changeBackground(data?.condition)
-          setLoading(false)
-        })
-      })
+          console.log(data);
+          setWeatherData(data);
+          changeBackground(data?.condition);
+          setLoading(false);
+        });
+      });
     }
-  }
-
+  };
 
   const getLiveLocation = async () => {
     try {
-      const data = await (await location.get("/json/")).data
+      const data = await (await location.get("/json/")).data;
       return {
         city: data.city,
         country: data.country_name,
         state: data.state,
-      }
-
+      };
     } catch (error) {
-      console.log(error.message)
+      console.log(error.message);
     }
-  }
+  };
 
   useEffect(() => {
     getLiveLocation().then((data) => {
-      setLocationInfo(data)
-    })
-
-  }, [setLocationInfo])
+      setLocationInfo(data);
+    });
+  }, [setLocationInfo]);
 
   useEffect(() => {
     getWeatherData(locationInfo.city).then((data) => {
       if (!data) {
-        return
+        return;
       }
-      console.log(data)
-      setWeatherData(data)
-      changeBackground(data?.condition)
-      setLoading(false)
-    })
+      console.log(data);
+      setWeatherData(data);
+      changeBackground(data?.condition);
+      setLoading(false);
+    });
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [locationInfo.city])
-
+  }, [locationInfo.city]);
 
   return (
     <>
@@ -121,33 +122,37 @@ function App() {
         containerStyle={{}}
         toastOptions={{
           // Define default options
-          className: '',
+          className: "",
           duration: 5000,
           style: {
-            background: '#363636',
-            color: '#fff',
+            background: "#363636",
+            color: "#fff",
           },
 
           // Default options for specific types
           success: {
             duration: 3000,
             theme: {
-              primary: 'green',
-              secondary: 'black',
+              primary: "green",
+              secondary: "black",
             },
           },
         }}
       />
       <div className="wrapper">
-        {loading ? "" : <Sidebar
-          clouds={weatherData.clouds}
-          condition={weatherData.condition}
-          tempUnit={"C"}
-          icons={getIcon(weatherData.condition)}
-          temperature={weatherData.temperature}
-          key={weatherData.city}
-          onChangeCity={getWeatherData}
-        />}
+        {loading ? (
+          ""
+        ) : (
+          <Sidebar
+            clouds={weatherData.clouds}
+            condition={weatherData.condition}
+            tempUnit={"C"}
+            icons={getIcon(weatherData.condition)}
+            temperature={weatherData.temperature}
+            key={weatherData.city}
+            onChangeCity={getWeatherData}
+          />
+        )}
         <div className="main">
           <nav>
             <ul className="options">
@@ -157,19 +162,23 @@ function App() {
           {<Forcast />}
           <div className="highlights">
             <h2 className="heading">today&apos;s highlights</h2>
-            {loading ? "" : <Highlights
-              Humidity={weatherData.humidity}
-              Sunrise={+weatherData.Sunrise}
-              Sunset={+weatherData.Sunset}
-              Visibility={weatherData.visibility}
-              WindStatus={+weatherData.windSpeed}
-              key={locationInfo.city}
-            />}
+            {loading ? (
+              ""
+            ) : (
+              <Highlights
+                Humidity={weatherData.humidity}
+                Sunrise={+weatherData.Sunrise}
+                Sunset={+weatherData.Sunset}
+                Visibility={weatherData.visibility}
+                WindStatus={+weatherData.windSpeed}
+                key={locationInfo.city}
+              />
+            )}
           </div>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
